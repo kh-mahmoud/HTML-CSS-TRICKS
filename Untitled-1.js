76d4e416ad96221ad73642c12738a2c5cde56de4
Khouchane Mahmoud <97807779+Uno486@users.noreply.github.com>
2022-02-22 18:40:14 +0100

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,15 @@
+function a()
+{
+ document.getElementById("bare").style.left="0 ";
+ document.getElementById("cancel").style.left="205 ";
+ document.getElementById("btn").style.left="250";
+ document.getElementById("btn").style.opacity="0";
+}
+function b()
+{
+ document.getElementById("bare").style.left="-250 ";
+ document.getElementById("cancel").style.left="-50 ";
+ document.getElementById("btn").style.left="40";
+ document.getElementById("btn").style.opacity="1";
+}
+
